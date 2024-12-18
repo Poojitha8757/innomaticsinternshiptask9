b2c8324568b9e90aadc09ab1b8f5ecbe1a4acf28
Poojitha1234 <121243570+Poojitha8757@users.noreply.github.com>
2024-12-18 16:18:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Load environment variables from .env file
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
